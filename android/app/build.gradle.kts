f12a9c8781ef5147c2332a3f1dac1b69cd370ecd
AKAI SWAT <mrmw2929@gmail.com>
2026-07-27 18:09:39 +0300

    Add build workflow

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.example.voice_our_iraq_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 22
+        minSdk = flutter.minSdkVersion
         targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
