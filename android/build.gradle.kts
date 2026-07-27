63de1807a06fb05efa98c11bda27951019d1eb11
AKAI SWAT <mrmw2929@gmail.com>
2026-07-27 16:04:21 +0300

    Force subprojects compileSdk 35

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,3 +22,14 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+subprojects {
+    afterEvaluate {
+        val project = this
+        if (project.hasProperty("android")) {
+            val android = project.extensions.findByName("android") as? com.android.build.gradle.BaseExtension
+            android?.let {
+                it.compileSdkVersion(35)
+            }
+        }
+    }
+}
